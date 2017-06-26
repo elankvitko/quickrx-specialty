01946844ed2de9aa0a36533f73e49952b05da22a
Elan Kvitko <elan@idknyc.com>
2017-06-26 19:15:10 -0400

    add styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,19 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-
-Rails.application.config.assets.precompile += %w( general.css )
-Rails.application.config.assets.precompile += %w( qrp_site_styles.css )
-Rails.application.config.assets.precompile += %w( qrp-header-additional-style.min.css )
-Rails.application.config.assets.precompile += %w( main.min.js )
-Rails.application.config.assets.precompile += %w( espark-main-script.min.js )
-Rails.application.config.assets.precompile += %w( espark-main-style.min.css )
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( main.css )
-Rails.application.config.assets.precompile += %w( owl.carousel.css )
-Rails.application.config.assets.precompile += %w( owl.theme.css )
-Rails.application.config.assets.precompile += %w( owl.transitions.css )
-Rails.application.config.assets.precompile += %w( responsive.css )
-Rails.application.config.assets.precompile += %w( bootstrap.min.js )
-Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
